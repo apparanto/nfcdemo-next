433e04006d891c06f1b4bda1a395c8a76b91e3c7
Robert Portier <robert.portier@apparanto.com>
2023-11-20 22:46:37 +0100

    Remove hardcoded NFC fail

--- src/app/api/nfc/route.ts
+++ src/app/api/nfc/route.ts
@@ -4,13 +4,9 @@ import UFR from "@/lib/nfc/ufr";
 export async function POST(request: Request) {
   const data: Tag = await request.json();
   console.log(data);
-  if (data.UID === "8D:AF:07:DF") {
-    const ufr = new UFR();
-    ufr.readerUISignal(ufr.UFR1, 1, 1);
-    ufr.onlineRGB(ufr.ONLINE, 0, 255, 0, 1500);
-    const response = ufr.getResponse;
-    console.log(response);
-    return new Response(response, { status: 200 });
-  }
-  return new Response("FAILED", { status: 400 });
+  const ufr = new UFR();
+  ufr.readerUISignal(ufr.UFR1, 1, 1);
+  ufr.onlineRGB(ufr.ONLINE, 0, 255, 0, 1500);
+  const response = ufr.getResponse;
+  return new Response(response, { status: 200 });
 }
