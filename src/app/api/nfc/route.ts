f3766ecf9d69d7ef5c65427057d4f944efa5e116
Robert Portier <robert.portier@apparanto.com>
2023-11-23 22:23:55 +0100

    add mongodb with mongoose and connect lib

--- src/app/api/nfc/route.ts
+++ src/app/api/nfc/route.ts
@@ -1,9 +1,13 @@
 import { NextResponse } from "next/server";
 import UFR from "@/lib/nfc/ufr";
+import dbConnect from "@/lib/mongodb/mongoose";
 
 export async function POST(request: Request) {
   const data: Tag = await request.json();
-  console.log(data);
+  const timestamp: Date = new Date();
+  console.log({ timestamp, data });
+  await dbConnect();
+
   const ufr = new UFR();
   ufr.readerUISignal(ufr.UFR1, 1, 1);
   ufr.onlineRGB(ufr.ONLINE, 0, 255, 0, 1500);
