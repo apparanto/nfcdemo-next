4e39c11cc4d1be4ea8da2975bf56a42583528032
Robert Portier <robert.portier@apparanto.com>
2023-11-25 22:47:21 +0000

    Add 'soon...'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        NFC demo with NextJS 14.x
+        NFC demo with NextJS 14.x.
+      </div>
+      <div>
+        <a href="#" className="hover:underline">Soon</a>...
       </div>
     </main>
   );
