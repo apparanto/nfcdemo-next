f3f797dbd4ebd0dcfcc1e3a4db7b6191031ec1ae
Robert Portier <robert.portier@apparanto.com>
2023-12-03 12:34:25 +0000

    load leaflet script async

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin="" />
-        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        <script async src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
           integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
           crossOrigin=""></script>
       </head>
