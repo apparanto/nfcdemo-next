f84acfb4873a32860e1c43f68c246d7fe12770a8
Robert Portier <robert.portier@apparanto.com>
2023-12-03 18:32:34 +0100

    make responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ const AxMap = dynamic(
 );
 
 export default async function Home() {
-  const realIp = headers().get('X-Real-IP');
-
+  const realIp = headers().get('X-Real-IP') || process.env.NEXT_PUBLIC_IP_ADDRESS;
+  console.log('realIp', realIp);
   const ipData: IpData | null = realIp ? await getIpData(realIp as string) : null;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 lg:p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        NFC demo with NextJS 14.x.
+        Application demo with NextJS 14.x.
         <div>
           <a href="#" className="hover:underline">Soon</a>...
         </div>
