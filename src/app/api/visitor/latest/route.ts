e36a06819a22e89f90a375a3ebf024241b22e33d
Robert Portier <robert.portier@apparanto.com>
2023-12-09 21:12:17 +0100

    Update visitor route to include cityName in select and increase limit to 200

--- src/app/api/visitor/latest/route.ts
+++ src/app/api/visitor/latest/route.ts
@@ -3,10 +3,10 @@ import { IVisitor } from "@/models/visitor/Visitor";
 import Visitor from "@/models/visitor/Visitor";
 import { NextResponse } from "next/server";
 
-const select = { latitude: 1, longitude: 1, lastVisit: 1, visitCount: 1, _id: 0 };
+const select = { latitude: 1, longitude: 1, lastVisit: 1, visitCount: 1, _id: 1, cityName: 1 };
 
 export async function GET() {
   await dbConnect();
-  const Visitors: IVisitor[] = await Visitor.find().sort({ lastVisit: -1 } as any).select(select).limit(50).exec();
+  const Visitors: IVisitor[] = await Visitor.find().sort({ lastVisit: -1 } as any).select(select).limit(200).exec();
   return NextResponse.json(Visitors);
 }
