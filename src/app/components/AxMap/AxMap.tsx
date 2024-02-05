3aeacf1fc455c9da5c4dbcb2e8fdd60c64653e97
Robert Portier <robert.portier@apparanto.com>
2024-02-05 19:42:14 +0100

    Refactor AxMap component and update map tiles styling

--- src/app/components/AxMap/AxMap.tsx
+++ src/app/components/AxMap/AxMap.tsx
@@ -1,13 +1,20 @@
-'use client'
+'use client';
 
 import { IVisitor } from '@/models/visitor/Visitor';
 import { Map } from 'leaflet';
 import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, CircleMarker, Popup, Tooltip } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  CircleMarker,
+  Popup,
+  Tooltip,
+} from 'react-leaflet';
 
 type Props = {
   v: IVisitor;
-}
+};
 
 export const AxMap = ({ v }: Props) => {
   const [isClient, setIsClient] = React.useState(false);
@@ -17,7 +24,7 @@ export const AxMap = ({ v }: Props) => {
   useEffect(() => {
     setIsClient(true);
     fetch('/api/visitor/latest')
-      .then(res => res.json())
+      .then((res) => res.json())
       .then((data: IVisitor[]) => {
         setVisitors(data);
       })
@@ -25,27 +32,34 @@ export const AxMap = ({ v }: Props) => {
   }, []);
 
   return (
-    <MapContainer ref={mapRef} center={[40, 0]} zoom={1.8} scrollWheelZoom={true} className='flex-auto w-full'>
+    <MapContainer
+      ref={mapRef}
+      center={[30, 0]}
+      zoom={2.5}
+      scrollWheelZoom={true}
+      className="flex-auto w-full">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        className="map-tiles"
       />
       <Marker position={[v.latitude!, v.longitude!]}>
         <Popup>
-          {v.visitCount === 1 ? 'You are new here!' : `You visited ${v.visitCount} times`}
+          {v.visitCount === 1
+            ? 'You are new here!'
+            : `You visited ${v.visitCount} times`}
         </Popup>
       </Marker>
-      {
-        visitors.map((v, i) => (
-          <CircleMarker key={i} center={[v.latitude!, v.longitude!]} radius={10}>
-            <Tooltip>
-              {v._id.city}, {v._id.country}: {v.visitCount} visit{v.visitCount > 1 ? 's' : ''}
-            </Tooltip>
-          </CircleMarker>
-        ))
-      }
+      {visitors.map((v, i) => (
+        <CircleMarker key={i} center={[v.latitude!, v.longitude!]} radius={10}>
+          <Tooltip>
+            {v._id.city}, {v._id.country}: {v.visitCount} visit
+            {v.visitCount > 1 ? 's' : ''}
+          </Tooltip>
+        </CircleMarker>
+      ))}
     </MapContainer>
-  )
-}
+  );
+};
 
 export default AxMap;
